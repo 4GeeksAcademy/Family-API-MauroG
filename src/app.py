8ab1129b4cc401829b13ef08922b843bfb5b88fb
MauroG07 <71202727+MauroG07@users.noreply.github.com>
2025-02-21 20:06:20 +0000

    EndPoints-Finished

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ from datastructures import FamilyStructure
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
-jackson_family = FamilyStructure("Jackson")  # Create the jackson family object
+jackson_family = FamilyStructure("Fuentes")  # Create the jackson family object
 
 
 # Handle/serialize errors like a JSON object
@@ -26,15 +26,45 @@ def sitemap():
     return generate_sitemap(app)
 
 
-@app.route('/members', methods=['GET'])
+@app.route('/members', methods=['GET', 'POST'])
 def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
     response_body = {}
-    members = jackson_family.get_all_members()
-    response_body['hello'] = "world"
-    response_body['family'] =  members
-    return response_body, 200
+    if request.method == 'GET':
+        members = jackson_family.get_all_members()
+        response_body['hello'] = "world"
+        response_body['last_name'] = jackson_family.get_last_name()
+        response_body['family'] =  members
+        return response_body, 202
+    if request.method == 'POST':
+        data = request.json
+        print(data)
+        jackson_family.add_member(data)
+        members = jackson_family.get_all_members()
+        response_body['message'] = "new family member"
+        response_body['family'] =  members
+        return response_body, 200
 
+
+@app.route('/members/<int:id>', methods={'GET', 'PUT', 'DELETE'})
+def member(id):
+    response_body = {}
+     
+    if request.method == 'GET':
+        row = jackson_family.get_member(id)
+        if row:
+            response_body['results'] = row
+            return response_body, 200
+        response_body['message'] = f'id {id} not found'
+        return response_body, 400
+    if request.method == 'PUT':
+        response_body['message'] = f'Respuesta para el PUT'
+        return response_body, 200
+    if request.method == 'DELETE':
+        rows = jackson_family.delete_member(id)
+        response_body['message'] = f'Respuesta para el DELETE'
+        response_body['results'] = rows
+        return response_body, 200
+    
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
